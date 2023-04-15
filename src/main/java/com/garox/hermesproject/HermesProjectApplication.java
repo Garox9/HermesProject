de8d5526195c4bf53ad2121e047769d0ef5b7cf7
Garox <Garox99@gmail.com>
2023-04-15 17:33:11 +0200

    STart Project

---
+++ src/main/java/com/garox/hermesproject/HermesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.garox.hermesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermesProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HermesProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/garox/hermesproject/HermesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garox.hermesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
